7c6dfae924905ccad7798d6c8a1f2de2351c64cb
VolodVakhn <kristina.lavrinok@gmail.com>
2026-03-18 12:14:31 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/atrist-portfolio/',
+  base: '/artist-portfolio/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
